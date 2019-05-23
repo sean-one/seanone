8724508b6e294469e86dab62beee92050fb37f19
sean-one <basement365@gmail.com>
2019-05-22 18:15:24 -0700

    add about container for styling

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -11,9 +11,12 @@ class Main extends Component {
         return (
             <div className="mainBody">
                 <h1>Sean Flannigan</h1>
-                <div className="aboutMe">
-                    <img src={Profile} alt="SeanOne profile" />
-                    <p>My name is Sean Flannigan and I am a Web Developer.  I have recently completed the online bootcamp, Lambda School.  I am focused on combining my last 15 years of marketing and promotions, with my new found passion for web development</p>
+                <div className="aboutContainer">
+                    <h3>Web Developer | California, USA</h3>
+                    <div className="aboutMe">
+                        <img src={Profile} alt="SeanOne profile" />
+                        <p>My name is Sean Flannigan and I am a Web Developer.  I have recently completed the online bootcamp, Lambda School.  I am focused on combining my last 15 years of marketing and promotions, with my new found passion for web development</p>
+                    </div>
                 </div>
             </div>
         );
