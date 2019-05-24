37e162cc4da13c7ca849b2d791bf4ce4dcbf6d9f
sean-one <basement365@gmail.com>
2019-05-23 21:01:18 -0700

    added social icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 // page sections
 import Header from './components/Header/Header';
@@ -7,6 +10,10 @@ import Main from './components/Main/Main';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/footer';
 
+// styling
+import './App.css';
+
+library.add(fab, faEnvelope);
 
 function App() {
   return (
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// styling
+import './Contact.css';
 
 class Contact extends Component {
     render() {
         return (
             <div className="contactSection">
                 <div className="socialLinks">
-                    
+                    <div className="emailLink">
+                        <FontAwesomeIcon icon="envelope" size="4x" />
+                    </div>
+                    <div className="linkedInLink">
+                        <FontAwesomeIcon icon={['fab', 'linkedin']} size="4x" />
+                    </div>
+                    <div className="githubLink">
+                        <FontAwesomeIcon icon={['fab', 'github']} size="4x" />
+                    </div>
+
 
                 </div>
             </div>
