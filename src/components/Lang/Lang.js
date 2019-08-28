fe5a74a05990708881f0b99fdca3f5900f029e28
SeanOne <basement365@gmail.com>
2019-08-27 23:13:35 -0700

    adjusted some of the about section & resize lang

--- src/components/Lang/Lang.js
+++ src/components/Lang/Lang.js
@@ -9,23 +9,23 @@ class Lang extends Component {
             <div className='languages'>
                 <div className='code'>
                     <div className='langIcon'>
-                        <FontAwesomeIcon icon={['fab', 'html5']} size="4x" />
+                        <FontAwesomeIcon icon={['fab', 'html5']} size="5x" />
                         <p>html</p>
                     </div>
                     <div className='langIcon'>
-                        <FontAwesomeIcon icon={['fab', 'css3-alt']} size="4x" />
+                        <FontAwesomeIcon icon={['fab', 'css3-alt']} size="5x" />
                         <p>css</p>
                     </div>
                     <div className='langIcon'>
-                        <FontAwesomeIcon icon={['fab', 'js-square']} size="4x" />
+                        <FontAwesomeIcon icon={['fab', 'js-square']} size="5x" />
                         <p>Javascript</p>
                     </div>
                     <div className='langIcon'>
-                        <FontAwesomeIcon icon={['fab', 'python']} size="4x" />
+                        <FontAwesomeIcon icon={['fab', 'python']} size="5x" />
                         <p>Python</p>
                     </div>
                     <div className='langIcon'>
-                        <FontAwesomeIcon icon={['fab', 'node-js']} size="4x" />
+                        <FontAwesomeIcon icon={['fab', 'node-js']} size="5x" />
                         <p>Node JS</p>
                     </div>
                 </div>
