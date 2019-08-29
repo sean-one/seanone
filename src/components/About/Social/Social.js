c3449ab0aa3a3a1525d38693c73b0e06e5276c3d
SeanOne <basement365@gmail.com>
2019-08-28 22:14:04 -0700

    added some styling and links to social buttons

--- src/components/About/Social/Social.js
+++ src/components/About/Social/Social.js
@@ -8,22 +8,26 @@ class Social extends Component {
     render() {
         return (
             <div className='socialWrapper'>
-                <div className='instagram'>
-                    <div className='icon'>
-                        <FontAwesomeIcon icon={['fab', 'instagram']} size="5x" />
+                <a href='https://www.instagram.com/senor_one_more/' target='blank'>
+                    <div className='instagram'>
+                        <div className='icon'>
+                            <FontAwesomeIcon icon={['fab', 'instagram']} size="6x" />
+                        </div>
+                        <div className='username'>
+                            <p>SENOR_ONE_MORE</p>
+                        </div>
                     </div>
-                    <div className='username'>
-                        <p>SENOR_ONE_MORE</p>
+                </a>
+                <a href='https://github.com/sean-one' target='blank'>
+                    <div className='github'>
+                        <div className='icon'>
+                            <FontAwesomeIcon icon={['fab', 'github']} size="6x" />
+                        </div>
+                        <div className='username'>
+                            <p>SEAN-ONE</p>
+                        </div>
                     </div>
-                </div>
-                <div className='github'>
-                    <div className='icon'>
-                        <FontAwesomeIcon icon={['fab', 'github']} size="5x" />
-                    </div>
-                    <div className='username'>
-                        <p>sean-one</p>
-                    </div>
-                </div>
+                </a>
             </div>
         )
     }
