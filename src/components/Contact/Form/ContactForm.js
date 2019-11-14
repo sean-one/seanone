f73b9f3e6c0ddfa618a574bbd99fd500b2bfffb1
SeanOne <basement365@gmail.com>
2019-11-14 15:56:42 -0800

    added contact form validation | finished send

--- src/components/Contact/Form/ContactForm.js
+++ src/components/Contact/Form/ContactForm.js
@@ -13,53 +13,122 @@ function ContactForm() {
     const [emailError, setEmailError] = useState('');
     const [phoneError, setPhoneError] = useState('');
     const [messageError, setMessageError] = useState('');
+    const [submitError, setSubmitError] = useState('');
 
-    const checkValid = () => {
-        if (!email.match(/\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi)) {
-            setEmailError('Please enter a valid email');
+    const validate = (e) => {
+        switch(e.name) {
+            case 'formName':
+                if (e.value.length < 1) {
+                    setNameError('Please be sure to include your name');
+                    break;
+                } else {
+                    setNameError('');
+                    break;
+                }
+            case 'formMessage':
+                if (e.value.length < 1) {
+                    setMessageError('Please include a brief project description');
+                    break;
+                } else {
+                    setMessageError('');
+                    break;
+                }
+            case 'formEmail':
+                if (!e.value.match(/\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi)) {
+                    setEmailError('Please include a valid email address');
+                    break;
+                } else {
+                    setEmailError('');
+                    break;
+                }
+            case 'formPhone':
+                if (!e.value.match(/(?:\d{1}\s)?\(?(\d{3})\)?-?\s?(\d{3})-?\s?(\d{4})/g)) {
+                    setPhoneError('Please include a valid phone number');
+                    break;
+                } else {
+                    setPhoneError('');
+                    break;
+                }
+            default:
+                console.log('somethings not right');
+                break;
         }
-        return true;
-
     }
 
     const handleSubmit = e => {
         e.preventDefault();
-        if (checkValid()) {
-
+        if (nameError === '' && emailError === '' && phoneError === '' && messageError === '') {
+            setSubmitError('');
+            Axios({
+                method : 'post',
+                url : 'https://f8oxshxeb5.execute-api.us-east-1.amazonaws.com/default/submit-contact',
+                data : {
+                    name,
+                    email,
+                    phone,
+                    message
+                }
+            });
+            setName('');
+            setEmail('');
+            setPhone('');
+            setMessage('');
         } else {
-            setEmailError('');
+            setSubmitError('Please be sure to complete all fields')
         }
-        // let isValidemail = email.match(/\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi);
-        // let isValidphone = phone.match(/[(]?\d{3}[-.)]?[ ]?\d{3}[-.]?\d{4}\b/g);
-        // console.log(isValidemail, isValidphone);
-        // Axios({
-        //     method : 'post',
-        //     url : 'https://f8oxshxeb5.execute-api.us-east-1.amazonaws.com/default/submit-contact',
-        //     data : {
-        //         name,
-        //         email,
-        //         phone,
-        //         message
-        //     }
-        // });
-        // console.log(e);
-        setName('');
-        setEmail('');
-        setPhone('');
-        setMessage('');
     }
 
     return (
         <form className='contactForm' onSubmit={handleSubmit} >
-            <p>Name:</p><div>{nameError}</div>
-            <input className='formInput' type='text' placeholder='John Doe' name='formName' value={name} onChange={e => setName(e.target.value)} /><br />
-            <p>Email:</p><div>{emailError}</div>
-            <input className='formInput' type='text' placeholder='name@email.com' name='formEmail' value={email} onChange={e => setEmail(e.target.value)} /><br />
+            <p>Name:</p>
+            <input 
+                className='formInput'
+                type='text'
+                placeholder='John Doe'
+                name='formName'
+                value={name}
+                onChange={e => setName(e.target.value)}
+                onBlur={e => validate(e.target)}
+            />
+            <div>{nameError}</div>
+            <br />
+            <p>Email:</p>
+            <input
+                className='formInput'
+                type='text'
+                placeholder='name@email.com'
+                name='formEmail'
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                onBlur={e => validate(e.target)}
+            />
+            <div>{emailError}</div>
+            <br />
             <p>Phone:</p>
-            <input className='formInput' type='text' placeholder='555-555-5555' name='formPhone' value={phone} onChange={e => setPhone(e.target.value)} /><br />
+            <input
+                className='formInput'
+                type='text'
+                placeholder='555-555-5555'
+                name='formPhone'
+                value={phone}
+                onChange={e => setPhone(e.target.value)}
+                onBlur={e => validate(e.target)}
+            />
+            <div>{phoneError}</div>
+            <br />
             <p>Message:</p>
-            <textarea className='formInput' name='formMessage' placeholder='Tell us a little about what you are looking for...' rows='10' cols='30' value={message} onChange={e => setMessage(e.target.value)} /><br />
-            <input className='formButton' type="submit" value="Submit" /><br />
+            <textarea
+                className='formInput'
+                name='formMessage'
+                placeholder='Tell us a little about what you are looking for...'
+                rows='10'
+                cols='30'
+                value={message}
+                onChange={e => setMessage(e.target.value)}
+                onBlur={e => validate(e.target)}
+            />
+            <br />
+            <input className='formButton' type="submit" value="Submit" /><div>{submitError}</div><br />
         </form>
     );
 
